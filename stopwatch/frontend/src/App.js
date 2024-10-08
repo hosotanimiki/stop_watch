0ec8b6b045859514409f87b5aadaf8ca9eb75206
細谷実樹 <hosotanimiki@DeMiA-001-0068noMacBook-Air.local>
2024-10-08 17:40:40 +0900

    ログインフォーム作成

--- stopwatch/frontend/src/App.js
+++ stopwatch/frontend/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import LoginForm from './components/LoginForm';
+
 function App() {
   return (
     <div className="App">
@@ -18,7 +21,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <LoginForm/>
+      </div>
     </div>
+    
   );
 }
 
